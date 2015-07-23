f9d0e5daa8142a3931621b1d56515cfafc7b53db
David Richardson <david@tankski.co.uk>
2015-07-23 21:32:35 +0100

    Added index.html and associated site controllers

--- app/__init__.py
+++ app/__init__.py
@@ -8,5 +8,9 @@ app.config.from_object('config')
 # Init database
 db = SQLAlchemy(app)
 
+from app.mod_site.controllers import mod_site as site_module
+
+app.register_blueprint(site_module)
+
 # Create all database schemas
 db.create_all()
---
+++ app/mod_site/controllers.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+mod_site = Blueprint('site', __name__)
+
+@mod_site.route('/', methods=['GET'])
+def home():
+    return render_template('site/index.html')

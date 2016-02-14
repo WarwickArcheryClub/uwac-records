8fa1713fea247a9146f873e3a7d8b76a61fbd9d4
David Richardson <david@tankski.co.uk>
2016-02-14 11:46:42 +0000

    Formatting

--- app/mod_admin/controllers.py
+++ app/mod_admin/controllers.py
@@ -1,19 +1,16 @@
+from datetime import date
 from threading import Thread
 from time import strptime, mktime
-from datetime import date
 
+import bcrypt
+import requests
+from app import app, db, mail
+from app.mod_site.controllers import is_integer, is_date, is_category, category_map
+from app.models import Users, QueuedScores, Scores, NewArchers, Archers, BowTypes, Rounds, Events
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, login_user, current_user, logout_user
 from flask_mail import Message
-from app.mod_site.controllers import is_integer, is_date, is_category, category_map
-from app.models import Users, QueuedScores, Scores, NewArchers, Archers, BowTypes, Rounds, Events
-from app import app, db, mail
 from genderize import Genderize
-import requests
-import bcrypt
-
-
-
 
 # Use the C ElementTree implementation where possible
 try:
